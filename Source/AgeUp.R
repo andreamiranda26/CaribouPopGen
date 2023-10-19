a6f9d576414b083d8223e2068baaaa7136ebaf80
andreamiranda26 <77080528+andreamiranda26@users.noreply.github.com>
2023-10-18 23:09:51 -0500

    Update AgeUp.R

--- Source/AgeUp.R
+++ Source/AgeUp.R
@@ -1,5 +1,5 @@
 #AgeUp.R for Caribou Genetics 
-#already tweaked !
+#already tweaked and debugged!!!
 
 AgeUp = function(pop, source){
   dead = pop[pop[,8] == 0, , drop=FALSE]                 #define dead indvs use the "alive" parameter in RunModel.R 
@@ -18,20 +18,21 @@ AgeUp = function(pop, source){
   }
   pop<- rbind(pop,dead)                                  #recall dead indvs
   remove(dead)                                           #clean up
-  return(pop)
+
+
+sdeadS = source[source[,8] == 0, , drop=FALSE]                 #define dead indvs use the "alive" parameter in RunModel.R 
+
+if(nrow(sdeadS)>= 1){
+  source = source[-which(source[,1]%in%sdeadS), , drop=FALSE]     #remove dead indvs
 }
+
+if(!is.null(nrow(source))){
+  source[,4] = as.numeric(source[,4]) + 1                                #add one year to all live indv's ages this is a two in RunModel.R
   
-  deadS = source[source[,8] == 0, , drop=FALSE]                 #define dead indvs use the "alive" parameter in RunModel.R 
-  
-  if(nrow(deadS)>= 1){
-    source = source[-which(source[,1]%in%deadS), , drop=FALSE]     #remove dead indvs
-  }
-  
-  if(!is.null(nrow(source))){
-    source[,4] = as.numeric(source[,4]) + 1                                #add one year to all live indv's ages this is a two in RunModel.R
-    
-  }
-  source<- rbind(source,deadS)                                  #recall dead indvs
-  remove(deadS)                                           #clean up
-  return(source)                                         #error at this point: Error: no function to return from, jumping to top level
+}
+source<- rbind(source,sdeadS)                                  #recall dead indvs
+remove(sdeadS)                                            #clean up
+
+return(pop,source)                                         #might give error at this point: Error: no function to return from, jumping to top level, ignore this error when debugging! 
+
 }
